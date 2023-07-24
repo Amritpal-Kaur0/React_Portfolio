6b9de19533616d7b01432764438096edffd440b0
Amrit1-kaur <amrit.gill3005@gmail.com>
2023-07-24 23:00:55 +0530

    Extra Code Deleted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import './assets/App.css';
 import React from "react";
-// import Nav from './Components/Home/Nav';
-// import Home from './Components/Home/Hero'
-// import About from './Components/About/About'
-// import Contact from './Components/Contact/Contact'
+
 import PortfolioContainer from "./Components/PortfolioContainer";
 
 const App = () => <PortfolioContainer />;
-
-// function App() {
-//   return (
-//     <div>
-//       <Nav/>
-// {/* <img className='relative' src={bg} alt="background"/> */}
-// <Home/>
-// <About/>
-// <Contact/>
-
-//     </div>
-//   );
-// }
 
 export default App;
