e76fd78048e656b47b52b353f2353ee828d626f8
Amrit1-kaur <amrit.gill3005@gmail.com>
2024-01-19 16:01:07 +0530

    Skills section updated

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,18 +1,32 @@
-
-
 import React from 'react';
+import Lottie from 'react-lottie';
+import animationData from '../../assets/Animation.json';
 import bg from '../../assets/bg.jpg';
 import Footer from '../footer';
 import Skills from './Skills'
 
 export default function About() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
   return (
-    <div>
+    <div> 
     <div id="About " className=" relative ">
-      <img src={bg} alt="background" className="w-full h-screen blur-[4px]" />
-      <div className=" box m-16 sm:m-46 absolute top-10 xs:top-45 left-10 right-10 bottom-26 sm:bottom-26 bg-white  flex items-center justify-center">
+      {/* <img src={bg} alt="background" className="w-full h-screen blur-[4px]" /> */}
       
-        <div className="p-4 m-6">
+      <div className=" box sm:m-46 absolute top-10 xs:top-45 left-10 right-10 bottom-26 sm:bottom-26   flex items-center justify-center">
+      
+        <div className="p-4 m-2">
+        <Lottie 
+    options={defaultOptions}
+      height={200}
+      width={200} opacity={0.2} />
           <h2 className="text-3xl flex justify-center font-bold mb-10">About Me</h2>
           <p className="about-para text-wrap text-slate-950 font-serif ">
             I am a passionate and dedicated full stack web developer, constantly seeking new challenges and opportunities to enhance my skills. With a solid foundation in both front-end and back-end technologies, I strive to create innovative and user-friendly web experiences.
@@ -21,10 +35,11 @@ export default function About() {
           <p className="about-para text-wrap text-slate-950 font-serif mb-20">
             What sets me apart is my ability to seamlessly integrate the front-end and back-end components, creating seamless and efficient web applications. I am experienced in RESTful API development and understand the importance of clean and scalable code architecture.
           </p>
+          <Skills/>
         </div>
       </div>
     </div>
-      <Skills/>
+    
       <Footer />
     </div>
   );
--- src/Components/Pages/Skills.js
+++ src/Components/Pages/Skills.js
@@ -1,63 +1,119 @@
 import React from 'react';
-import skillbg from '../../assets/skillsbg.avif'
 
-const SkillsTree = () => {
-  const skills = {
-    // name: 'Root',
-    children: [
-      {
-        name: 'Frontend',
-        children: [
-          { name: 'HTML' },
-          { name: 'CSS' },
-          { name: 'JavaScript' },
-          { name: 'React' },
-          { name: 'PWA' },
-        ],
-      },
-      {
-        name: 'Backend',
-        children: [
-          { name: 'Express' },
-          { name: 'Node.js' },
-          { name: 'GraphQL' },
-          { name: 'SQL'
-           },
-          {
-            name: 'NoSQL',
-          },
-        ],
-      },
-      {
-        name: 'OOPs',
-      },
-    ],
-  }
-  
-
-
-  
-  const renderSkill = (skill) => {
-    return (
-      <li   key={skill.name} className=" box py-2 relative font-bold text-lg   flex  flex-wrap rounded justify-evenly">
-        {skill.name}
-        {skill.children && (
-          <ul className="pl-4  relative ">{skill.children.map((child) => renderSkill(child))}
-          </ul>
-        )}
-      </li>
-    );
-  };
-
+const Skills = () => {
   return (
-    <div className="skills-tree box relative ">
-        <img src={skillbg} alt="background" className=" absolute w-full h-screen blur-[2px] hover:blur-[6px]" />
-      <h1 className="text-2xl font-bold  text-center left-5 md:left-20 absolute  p-4 btn  overline ">My Skills 👉</h1>
-      <ul  className=' p-6  '>  {renderSkill(skills)} </ul>
+    <div className="max-w-2xl mx-auto p-4 mb-24">
+      <h2 className="text-3xl font-bold mb-4">Skills and Technologies</h2>
+
+      <div className="mb-6">
+        <h3 className="text-xl font-semibold mb-2">Languages</h3>
+        <p>JavaScript, HTML, CSS</p>
+      </div>
+
+      <div className="mb-6">
+        <h3 className="text-xl font-semibold mb-2">Front-end</h3>
+        <p>React, React Router, Redux, Recoil, SASS, Bootstrap, TailwindCSS, Handlebars, EJS</p>
+      </div>
+
+      <div className="mb-6">
+        <h3 className="text-xl font-semibold mb-2">Backend</h3>
+        <p>Node.js, Express.js, REST APIs, MVC, ORM</p>
+      </div>
+
+      <div className="mb-6">
+        <h3 className="text-xl font-semibold mb-2">Databases</h3>
+        <p>SQL, MongoDB</p>
+      </div>
+
+      <div className="mb-6">
+        <h3 className="text-xl font-semibold mb-2">Authorization and Validation</h3>
+        <p>JWT, Zod (for validation)</p>
+      </div>
+
+      <div className="mb-6">
+        <h3 className="text-xl font-semibold mb-2">Version Control</h3>
+        <p>Git, GitHub, Gitlab</p>
+      </div>
+
+      <div className="mb-6">
+        <h3 className="text-xl font-semibold mb-2">Deployment</h3>
+        <p>Heroku, Vercel</p>
+      </div>
+      <div className="mb-6">
+        <h3 className="text-xl font-semibold mb-2">Other Tools</h3>
+        <p>Ms Office , Lighthouse , PageSpeed Insights , Agile Methodologies</p>
+      </div>
+      <div className="mb-6">
+        <h3 className="text-xl font-semibold mb-2">Soft Skills</h3>
+        <p>Communication, Critical thinking & Problem-solving , collaboration , quick -learner.</p>
+      </div>
+
     </div>
   );
 };
-export default SkillsTree;
+
+export default Skills;
+
+// import React from 'react';
+// import skillbg from '../../assets/skillsbg.avif'
+
+// const SkillsTree = () => {
+//   const skills = {
+//     // name: 'Root',
+//     children: [
+//       {
+//         name: 'Frontend',
+//         children: [
+//           { name: 'HTML' },
+//           { name: 'CSS' },
+//           { name: 'JavaScript' },
+//           { name: 'React' },
+//           { name: 'PWA' },
+//         ],
+//       },
+//       {
+//         name: 'Backend',
+//         children: [
+//           { name: 'Express' },
+//           { name: 'Node.js' },
+//           { name: 'GraphQL' },
+//           { name: 'SQL'
+//            },
+//           {
+//             name: 'NoSQL',
+//           },
+//         ],
+//       },
+//       {
+//         name: 'OOPs',
+//       },
+//     ],
+//   }
+  
+
+
+  
+//   const renderSkill = (skill) => {
+//     return (
+//       <li   key={skill.name} className=" box py-2 relative font-bold text-lg   flex  flex-wrap rounded justify-evenly">
+//         {skill.name}
+//         {skill.children && (
+//           <ul className="pl-4  relative ">{skill.children.map((child) => renderSkill(child))}
+//           </ul>
+//         )}
+//       </li>
+//     );
+//   };
+
+//   return (
+//     <div className="skills-tree box relative ">
+//         <img src={skillbg} alt="background" className=" absolute w-full h-screen blur-[2px] hover:blur-[6px]" />
+//       <h1 className="text-2xl font-bold  text-center left-5 md:left-20 absolute  p-4 btn  overline ">My Skills 👉</h1>
+//       <ul  className=' p-6  '>  {renderSkill(skills)} </ul>
+//     </div>
+//   );
+// };
+// export default SkillsTree;
 
 
 
